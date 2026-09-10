f78c856b5288d95d64239259bc7a31903e50646d
nikoester-ui <alexandrunikos07@gmail.com>
2026-09-10 14:20:19 +0200

    Neon Ping Arena v1.07

---
+++ leaderboard-config.js
@@ -0,0 +1,6 @@
+// Neon Ping Arena global leaderboard configuration.
+// Create a Supabase project, then paste these values from the Project Connect dialog.
+window.NPA_LEADERBOARD = {
+  url: "YOUR_SUPABASE_PROJECT_URL",
+  publishableKey: "YOUR_SUPABASE_PUBLISHABLE_KEY"
+};
--- sw.js
+++ sw.js
@@ -1,6 +1,9 @@
-const CACHE='neon-ping-v5';
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./coin.png", "./shop-white.png", "./theme-space.png", "./theme-jungle.png", "./theme-lava.png", "./theme-ice.png", "./theme-cyber.png", "./ball-classic.png", "./ball-fire.png", "./ball-ice.png", "./ball-eight.png", "./ball-plasma.png", "./skull-easy.png", "./skull-normal.png", "./skull-hard.png", "./skull-impossible.png", "./boss-skull.png"];
-
+const CACHE='neon-ping-arena-1.07';
+const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./leaderboard-config.js", "./ball-classic.png", "./ball-eight.png", "./ball-fire.png", "./ball-ice.png", "./ball-plasma.png", "./boss-skull-0.png", "./boss-skull-1.png", "./boss-skull-2.png", "./boss-skull-3.png", "./boss-skull-4.png", "./boss-skull.png", "./coin.png", "./icon-192.png", "./icon-512.png", "./shop-white.png", "./skull-easy.png", "./skull-hard.png", "./skull-impossible.png", "./skull-normal.png", "./theme-cyber.png", "./theme-ice.png", "./theme-jungle.png", "./theme-lava.png", "./theme-space.png"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{let c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html')))));
+self.addEventListener('fetch',e=>{
+  const u=new URL(e.request.url);
+  if(u.hostname.endsWith('.supabase.co')){e.respondWith(fetch(e.request));return;}
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(x=>x||caches.match('./index.html'))));
+});
